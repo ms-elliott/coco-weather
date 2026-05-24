4fdfbb872bc5a274ce51bfb8dbea6a5868d07662
ms-elliott <ms.elliott55@gmail.com>
2026-05-24 18:29:34 +0900

    Fixed start script

--- backend/index.ts
+++ backend/index.ts
@@ -10,30 +10,30 @@ app.use(cors());
 
 const PORT = 3000;
 
-// app.get("/", (req, res) => {
-//     res.send("API OK");
-// });
+app.get("/", (req, res) => {
+    res.send("API OK");
+});
 
-// app.get("/api/weather", async (req, res) => {
-//     try {
-//         const raw = await fetchJmaForecast("130000"); // 東京都
-//         const data = transform(raw);
+app.get("/api/weather", async (req, res) => {
+    try {
+        const raw = await fetchJmaForecast("130000"); // 東京都
+        const data = transform(raw);
 
-//         res.json(data);
-//     } catch (e) {
-//         console.error(e);
-//         res.status(500).json({ error: "failed" });
-//     }
-// });
+        res.json(data);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: "failed" });
+    }
+});
 
-// app.get("/api/overview", async (req, res) => {
-//     try {
-//         const data = await fetchOverview();
-//         res.json(data);
-//     } catch (error) {
-//         res.status(500).json({ error: "取得失敗" });
-//     }
-// });
+app.get("/api/overview", async (req, res) => {
+    try {
+        const data = await fetchOverview();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "取得失敗" });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
