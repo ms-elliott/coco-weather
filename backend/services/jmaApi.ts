218ded72f662bd48e6ebc934c9f8887a2eaf85a0
ms-elliott <ms.elliott55@gmail.com>
2026-05-25 18:19:27 +0900

    Added debug codes

--- backend/services/jmaApi.ts
+++ backend/services/jmaApi.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export async function fetchJmaForecast(areaCode: string) {
     const url = `https://www.jma.go.jp/bosai/forecast/data/forecast/${areaCode}.json`;
+    console.log("🔥 FETCH URL:", areaCode);
 
     const response = await axios.get(url);
 
